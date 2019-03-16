e2398f88681670107f18c41d4aefcf694efaafe8
yash4patel <48629442+yash4patel@users.noreply.github.com>
2019-03-16 23:35:03 +0530

    Add files via upload

---
+++ TOH.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+void TOH(int ,char ,char ,char );
+
+int main()
+
+{
+   
+ int t;
+
+    char A,B,C;
+  
+  cin>>t;
+ 
+   TOH(t,'A','B','C');
+
+}
+
+void TOH(int t,char a,char b,char c)
+
+{
+
+    if(t>0)
+
+    {
+
+        TOH(t-1,a,c,b);
+
+        cout<<"move disk from"<< a <<"to"<< c <<"\n";
+
+        TOH(t-1,b,a,c);
+
+    }
+
+}
